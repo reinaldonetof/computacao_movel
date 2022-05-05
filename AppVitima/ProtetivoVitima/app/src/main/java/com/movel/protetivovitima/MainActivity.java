455434b29d2c622fe3c59a65db52faeacd670694
reinaldo.neto <reinaldo.neto@lsdi.ufma.br>
2022-05-04 23:12:38 -0300

    Clean app vitima

--- AppVitima/ProtetivoVitima/app/src/main/java/com/movel/protetivovitima/MainActivity.java
+++ AppVitima/ProtetivoVitima/app/src/main/java/com/movel/protetivovitima/MainActivity.java
@@ -40,13 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     CDDL cddl;
     private TextView messageTextView;
+    private TextView messageLatitude;
+    private TextView messageLongitude;
     private EditText editTextView;
     private View subscribeButton;
-    private Button sendButton;
     private ConnectionImpl con;
     private ConnectionImpl external_con;
     private Double latitude = 0.0;
     private Double longitude = 0.0;
+    private Double external_latitude = 0.0;
+    private Double external_longitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,34 +57,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setViews();
 
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-
-//        if(!checkPermission()){
-//            requestPermission();
-//        }
-
-        // Este codigo comentado envolve o uso seguro do CDDL. Estes métodos não devem ser chamados de forma sequencial.
-        // Caso alguma equipe deseje usar o CDDL no modo seguro me procure que eu explico melhor (andre.cardoso@lsdi.ufma.br)
-//        SecurityService securityService = new SecurityService(getApplicationContext());
-//        securityService.generateCSR("andre.cardoso","LSDi","UFMA","SLZ","MA","BR","andre.cardoso@lsdi.ufma.br");
-//        securityService.grantWritePermissionByCDDLTopic("andre.cardoso@lsdi.ufma.br", SecurityService.ALL_TOPICS);
-//        securityService.grantReadPermissionByCDDLTopic("andre.cardoso@lsdi.ufma.br", SecurityService.ALL_TOPICS);
-//        setCertificates("rootCA.crt", "client.crt", "ca_lsdi");
+        if(!checkPermission()){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
 
         subscribeButton.setOnClickListener(clickListener);
-        sendButton.setOnClickListener(sendListener);
     }
 
     private void setViews() {
         editTextView = findViewById(R.id.editText);
         subscribeButton = findViewById(R.id.subscribeButton);
         messageTextView = findViewById(R.id.messageTexView);
-        sendButton = findViewById(R.id.sendButton);
+        messageLatitude = findViewById(R.id.messageLatitude);
+        messageLongitude = findViewById(R.id.messageLongitude);
     }
 
     private void initCDDL(String clientId) {
         String host = CDDL.startMicroBroker();
-        //String host = CDDL.startSecureMicroBroker(getApplicationContext(), true)
         con = ConnectionFactory.createConnection();
         con.setClientId(clientId);
         con.setHost(host);
@@ -94,22 +86,12 @@ public class MainActivity extends AppCompatActivity {
         external_con.setHost(host_external);
         external_con.addConnectionListener(connectionListenerExternal);
         external_con.connect();
-//        con.secureConnect(getApplicationContext());
         cddl = CDDL.getInstance();
         cddl.setConnection(con);
         cddl.setContext(this);
         cddl.startService();
         cddl.startLocationSensor();
         cddl.startCommunicationTechnology(CDDL.INTERNAL_TECHNOLOGY_ID);
-    }
-
-    private void publishMessage() {
-        Publisher publisher = PublisherFactory.createPublisher();
-        publisher.addConnection(cddl.getConnection());
-        MyMessage message = new MyMessage();
-        message.setServiceName("my_service");
-        message.setServiceByteArray("Valor");
-        publisher.publish(message);
     }
 
     private void publishExternal(Double lat, Double lon) {
@@ -132,13 +114,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMessageArrived(Message message) {
                 if (message.getServiceName().equals("my_service")) {
-                    Log.d("_MAIN_EXTERNAL_MY_SVC", "+++" + message.toString());
-                }
-                if (message.getServiceName().equals("alert")) {
-                    Log.d("_MAIN_EXTERNAL_ALERTA", "+++" + message.toString());
+                    Log.d("_MAIN_EXTERNAL", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + message.toString());
+
                 }
                 if (message.getServiceName().equals("location_external")) {
-                    Log.d("_MAIN_EXTERNAL_LOCATION", "+++" + message.toString());
+                    String[] arrOfPoints = message.getServiceValue()[0].toString().split(";");
+                    external_latitude = Double.parseDouble(arrOfPoints[0]);
+                    external_longitude = Double.parseDouble(arrOfPoints[1]);
+                    messageLatitude.setText(external_latitude.toString());
+                    messageLongitude.setText(external_longitude.toString());
                 }
                 Log.d("_MAIN_EXTERNAL", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + message);
             }
@@ -213,13 +197,6 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    private View.OnClickListener sendListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            publishExternal(latitude, longitude);
-        }
-    };
-
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -227,7 +204,6 @@ public class MainActivity extends AppCompatActivity {
             initCDDL(et);
             subscribeMessage();
             subscribeExternalMessage(et);
-            thread.start();
         }
     };
 
@@ -271,20 +247,6 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, 1);
         }
     }
-
-    Thread thread = new Thread() {
-        @Override
-        public void run() {
-            try {
-                while(true) {
-                    sleep(1000);
-                    publishExternal(latitude, longitude);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    };
 
     private void setCertificates(String caFileName, String certFileName, String caAlias) {
         if (!checkPermission()) {
